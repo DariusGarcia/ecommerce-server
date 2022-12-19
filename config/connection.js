2f8c0fba8e4e1a7937a89b81f5214ee42de6c014
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-19 14:00:14 -0800

    sync sequelize to db

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,17 @@ const Sequelize = require('sequelize')
 
 const sequelize = process.env.JAWSDB_URL
 	? new Sequelize(process.env.JAWSDB_URL)
-	: new Sequelize('customers', 'root', 'Slugger132435!', {
-			host: 'localhost',
-			dialect: 'mysql',
-			dialectOptions: {
-				decimalNumbers: true,
-			},
-	  })
+	: new Sequelize(
+			process.env.DB_NAME,
+			process.env.DB_USERNAME,
+			process.env.DB_PASSWORD,
+			{
+				host: 'localhost',
+				dialect: 'mysql',
+				dialectOptions: {
+					decimalNumbers: true,
+				},
+			}
+	  )
 
 module.exports = sequelize
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+require('dotenv').config()
+const express = require('express')
+const routes = require('./routes')
+const sequelize = require('./config/connection')
+const app = express()
+const PORT = process.env.PORT || 3001
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(routes);
+app.use(routes)
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sequelize.sync({ force: true }).then(() => {
+	app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
+})
