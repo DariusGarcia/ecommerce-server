23d0387ccf68542cf9409d9590d680f62d7d9f91
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-19 20:27:26 -0800

    added relationships between data tables

--- models/index.js
+++ models/index.js
@@ -1,20 +1,37 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require('./Product')
+const Category = require('./Category')
+const Tag = require('./Tag')
+const ProductTag = require('./ProductTag')
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+	foreignKey: 'category_id',
+	onDelete: 'CASCADE',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+	foreignKey: 'category_id',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+	through: ProductTag,
+	as: 'product_tags',
+	foreignKey: 'product_id',
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+	through: ProductTag,
+	as: 'product_tags',
+	foreignKey: 'tag_id',
+})
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+	Product,
+	Category,
+	Tag,
+	ProductTag,
+}
