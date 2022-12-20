487c6922d47e34b6c45b733443327e917e9b7ec1
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-19 23:04:08 -0800

    fixed seed data

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -1,38 +1,43 @@
-const { Product } = require('../models');
+const { Product } = require('../models')
 
 const productData = [
-  {
-    product_name: 'Plain T-Shirt',
-    price: 14.99,
-    stock: 14,
-    category_id: 1,
-  },
-  {
-    product_name: 'Running Sneakers',
-    price: 90.0,
-    stock: 25,
-    category_id: 5,
-  },
-  {
-    product_name: 'Branded Baseball Hat',
-    price: 22.99,
-    stock: 12,
-    category_id: 4,
-  },
-  {
-    product_name: 'Top 40 Music Compilation Vinyl Record',
-    price: 12.99,
-    stock: 50,
-    category_id: 3,
-  },
-  {
-    product_name: 'Cargo Shorts',
-    price: 29.99,
-    stock: 22,
-    category_id: 2,
-  },
-];
+	{
+		name: 'Plain T-Shirt',
+		description: 'test',
+		price: 14.99,
+		in_stock: 14,
+		category_id: 1,
+	},
+	{
+		name: 'Running Sneakers',
+		description: 'test',
+		price: 90.0,
+		in_stock: 25,
+		category_id: 5,
+	},
+	{
+		name: 'Branded Baseball Hat',
+		description: 'test',
+		price: 22.99,
+		in_stock: 12,
+		category_id: 4,
+	},
+	{
+		name: 'Top 40 Music Compilation Vinyl Record',
+		description: 'test',
+		price: 12.99,
+		in_stock: 50,
+		category_id: 3,
+	},
+	{
+		name: 'Cargo Shorts',
+		description: 'test',
+		price: 29.99,
+		in_stock: 22,
+		category_id: 2,
+	},
+]
 
-const seedProducts = () => Product.bulkCreate(productData);
+const seedProducts = () => Product.bulkCreate(productData)
 
-module.exports = seedProducts;
+module.exports = seedProducts
